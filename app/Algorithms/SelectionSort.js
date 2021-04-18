780740d2df9b32dc05dce75cdcb1c0e30fb8ff1b
Lucas Mariano <lucas.carmo@localiza.com>
2021-04-18 16:35:11 -0300

    update

--- app/Algorithms/SelectionSort.js
+++ app/Algorithms/SelectionSort.js
@@ -1,13 +1,12 @@
 class SelectionSort extends SortingAlgorithm {
 
     
-
+    i = 0;
     constructor(values) {
         super(values);
     }
 
     step() {
-        this.i = 0;
         for (let j = this.i + 1; j < values.length; j++) {
                 if (values[this.i] > values[j]) {
                     super.CountComparisons();
